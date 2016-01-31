23b4172d62414e61f6c0c427d15482c453121d16
user <email@academiahack.com.ve@4geeks.co>
2016-01-31 10:08:10 +0000

    helper class

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,7 +1,23 @@
 module GamesHelper
     def caesar_print rule
       tabla=""
-      tabla="#{tabla}<h2>Leyenda Criptgráfica opcional</h2>"
+      library="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      shifted_library=""
+      library.each_char do |letter|
+            shifted_library << library[library.index(letter) - rule]
+      end
+
+      str = "<table>\n<tr><th>Original </th>"
+      library.each_char do |line|
+      	str += "<td> #{line} </td>\n"
+      end
+      str += "</tr><tr><th>Cesar "+rule.to_s+ "</th>"
+      shifted_library.each_char do |line|
+      	str += "<td> #{line} </td>\n"
+      end
+      str += "</tr></talbe>\n"
+
+      tabla="<h2>Leyenda Criptgráfica opcional</h2><br>#{str}"
 
       raw tabla
     end
