b2a3fa5327bcf726ed95a2f645b3f0f4641f6787
user <email@academiahack.com.ve@4geeks.co>
2016-01-31 02:14:42 +0000

    fix pequenios de posicion, y de css en la vsta del minion

--- app/controllers/minions_controller.rb
+++ app/controllers/minions_controller.rb
@@ -28,12 +28,14 @@ class MinionsController < ApplicationController
     end
 
     redirect_to game_links_path(game)
+
+    redirect_to minions_show_path(code)
   end
 
   def show
     @minion=Minion.find params[:id]
     @fake_words=[]
-    @fake_words << Word.get_random_record_without(@minion.game_word.word.name)
-    @fake_words << Word.get_random_record_without(@minion.game_word.word.name)
+    @fake_words << Word.get_random_record_without([@minion.game_word.word.name])
+    @fake_words << Word.get_random_record_without([@minion.game_word.word.name]+@fake_words)
   end
 end
