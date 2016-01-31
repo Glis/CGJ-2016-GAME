e556e7c95097388f36701fb068fae5d2591fb30e
user <email@academiahack.com.ve@4geeks.co>
2016-01-31 15:51:15 +0000

    validando los nombres en el controller de minion

--- app/assets/javascripts/minions.js
+++ app/assets/javascripts/minions.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
-  $(".minions:first").focus();
+    $(".minions:first").focus();
+
+    //var $namesSubmit = $('.names-submit');
+    //$('.minions');
+    //
+    //if($namesSubmit.length) {
+    //    $namesSubmit.keydown(function(e) {
+    //        if(e.keyCode === 13) {
+    //            if()
+    //
+    //            e.preventDefault();
+    //            $namesSubmit.click();
+    //            return false;
+    //        } else if (!(e.keyCode >= 48 && e.keyCode <= 90)) {
+    //            e.preventDefault();
+    //            return false;
+    //        }
+    //    });
+    //}
 });
--- app/controllers/minions_controller.rb
+++ app/controllers/minions_controller.rb
@@ -8,27 +8,43 @@ class MinionsController < ApplicationController
   def create
     #NO TIENE VISTA!
     names = params[:names]
-    game= Game.find params[:id]
 
-    minions_count = Minion.where(game_id: game).count
-
-    names.length.times do |i|
-      active_game_word=GameWord.create(game:game, word:Word.get_random_record)
-      minion = Minion.find_by name:names[i.to_s]
-      if minions_count < game.minions_quantity
-        minion = Minion.new unless minion
+    if !names.blank?
+      empty_name=false
+      names.each do |n|
+        if n[1].blank?
+          empty_name=true
+        end
       end
 
-      if minion
-        minion.name = names[i.to_s]
-        minion.game = game
-        minion.game_word = active_game_word
-        minion.position = i+1
-        minion.save
+      if empty_name
+        redirect_to :back
+      else
+        game= Game.find params[:id]
+
+        minions_count = Minion.where(game_id: game).count
+
+        names.length.times do |i|
+          active_game_word=GameWord.create(game:game, word:Word.get_random_record)
+          minion = Minion.find_by name:names[i.to_s]
+          if minions_count < game.minions_quantity
+            minion = Minion.new unless minion
+          end
+
+          if minion
+            minion.name = names[i.to_s]
+            minion.game = game
+            minion.game_word = active_game_word
+            minion.position = i+1
+            minion.save
+          end
+        end
+        redirect_to game_links_path(game)
       end
+
+    else
+      redirect_to :back
     end
-
-    redirect_to game_links_path(game)
   end
 
   def show
