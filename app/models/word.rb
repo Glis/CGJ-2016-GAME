8fafeaeb34ca9cb40dce18caf589a5efa2bae2a6
Romer Ramos <romerramos@gmail.com>
2016-02-01 18:42:09 -0430

    Fix posgres a mysql

--- app/models/word.rb
+++ app/models/word.rb
@@ -11,11 +11,11 @@
 class Word < ActiveRecord::Base
 
   def self.get_random_record
-    self.order("RANDOM()").first
+    self.order("RAND()").first
   end
 
   def self.get_random_record_without words
-    self.where("name NOT IN (?)", words).order("RANDOM()").first
+    self.where("name NOT IN (?)", words).order("RAND()").first
   end
 
 end
